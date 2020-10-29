8292eceb141cd36e016064796928befe7d3fde63
Nonato <rf.nonato@gmail.com>
2020-10-29 09:01:49 -0600

    add others path in API

--- handler.js
+++ handler.js
@@ -8,7 +8,7 @@ module.exports.create = (event, context, callback) => {
 
   connectToDatabase()
     .then(() => {
-      Note.create(JSON.parse(event.body))
+      user.create(JSON.parse(event.body))
         .then(user => callback(null, {
           statusCode: 200,
           body: JSON.stringify(user)
@@ -26,10 +26,10 @@ module.exports.getOne = (event, context, callback) => {
 
   connectToDatabase()
     .then(() => {
-      Note.findById(event.pathParameters.id)
-        .then(note => callback(null, {
+      user.findById(event.pathParameters.id)
+        .then(user => callback(null, {
           statusCode: 200,
-          body: JSON.stringify(note)
+          body: JSON.stringify(user)
         }))
         .catch(err => callback(null, {
           statusCode: err.statusCode || 500,
@@ -44,10 +44,10 @@ module.exports.getAll = (event, context, callback) => {
 
   connectToDatabase()
     .then(() => {
-      Note.find()
-        .then(notes => callback(null, {
+      user.find()
+        .then(user => callback(null, {
           statusCode: 200,
-          body: JSON.stringify(notes)
+          body: JSON.stringify(user)
         }))
         .catch(err => callback(null, {
           statusCode: err.statusCode || 500,
@@ -62,10 +62,10 @@ module.exports.update = (event, context, callback) => {
 
   connectToDatabase()
     .then(() => {
-      Note.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
-        .then(note => callback(null, {
+      user.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
+        .then(user => callback(null, {
           statusCode: 200,
-          body: JSON.stringify(note)
+          body: JSON.stringify(user)
         }))
         .catch(err => callback(null, {
           statusCode: err.statusCode || 500,
@@ -80,10 +80,10 @@ module.exports.delete = (event, context, callback) => {
 
   connectToDatabase()
     .then(() => {
-      Note.findByIdAndRemove(event.pathParameters.id)
-        .then(note => callback(null, {
+      user.findByIdAndRemove(event.pathParameters.id)
+        .then(user => callback(null, {
           statusCode: 200,
-          body: JSON.stringify({ message: 'Removed note with id: ' + note._id, note: note })
+          body: JSON.stringify({ message: 'Removed note with id: ' + user._id, note: user })
         }))
         .catch(err => callback(null, {
           statusCode: err.statusCode || 500,
